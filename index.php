929d09fcab3c1f7111a0ab956445780ae30643ef
IscJlchavezG <joseluis@iscjlchavezg.mx>
2023-05-19 21:49:09 -0600

    maquetado de login

--- index.php
+++ index.php
@@ -1,11 +1,89 @@
+<?php
+    include "includes/ConectBd.php";
+
+?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Inicio de Sistema</title>
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    </head>
-    <body>
-        <h1 class="text-center">Hola Bienvenido al mundo de desarrollo web</h1>
-    <script src="js/bootstrap.min.js"></script>
-    </body>
+
+<head>
+    <title>Inicio de Sistema | IscjlchavezG</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/dark.css">
+    <script src="js/jquery-3.7.0.min.js"></script>
+</head>
+
+<body>
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-10 col-lg-6 px-2 mt-3">
+            <h3 class="mb-4 text-center mt-3">Inicio de sesión</h3>
+
+            <div class="row py-1">
+                <div class="col"></div>
+                <div class="col"></div>
+                <div class="col">
+                    <div class="form-check form-switch">
+                        <input type="checkbox" id="darkSwitch" class="form-check-input">
+                        <label for="darkSwitch" class="form-check-label">
+                            <svg class="bi" width="22" height="22" fill="currentColor">
+                                <use xlink:href="library/icons/bootstrap-icons.svg#brightness-low" />
+                            </svg>
+                        </label> |
+                        <svg class="bi" width="15" height="15" fill="currentColor">
+                            <use xlink:href="library/icons/bootstrap-icons.svg#moon-stars" />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="FormLogin" class="px-3">
+                <div class="input-group mb-3 mt-1">
+                    <span class="input-group-text" id="User">
+                        <svg class="bi" width="15" height="15" fill="currentColor">
+                            <use xlink:href="library/icons/bootstrap-icons.svg#person-fill-check" />
+                        </svg>
+                    </span>
+                    <input type="text" name="usuario" class="form-control" placeholder="Usuario" aria-label="Usuario" required />
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="Pas">
+                        <svg class="bi" width="15" height="15" fill="currentColor">
+                            <use xlink:href="library/icons/bootstrap-icons.svg#lock-fill" />
+                        </svg>
+                    </span>
+                    <input type="password" name="password" class="form-control" placeholder="Password" id="VerPassWord" aria-label="Password" required />
+                </div>
+                <div class="row py-1 px-1">
+                    <div class="col">
+                        <div class="form-check form-switch">
+                            <input type="checkbox" id="VerPass" class="form-check-input" onclick="verPass(this);">
+                            <label for="VerPass" class="form-check-label">
+                                Visualizar Password
+                            </label>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <svg class="bi" width="15" height="15" fill="currentColor">
+                            <use xlink:href="library/icons/bootstrap-icons.svg#key-fill" />
+                        </svg>&nbsp;&nbsp;¿Perdiste tu Password?
+                    </div>
+                </div>
+                <div class="d-grid gap-2 mt-1">
+                    <input type="submit" name="BtnLogin" value="Ingresar" class="btn btn-sm btn-success rounded-pill">
+                </div>
+            </form>
+
+
+
+        </div>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/dark-mode.js"></script>
+        <script>
+            function verPass(ck){
+             if(ck.checked)
+               $('#VerPassWord').attr("type","text");
+              else
+              $('#VerPassWord').attr("type","password");
+            }
+        </script>
+</body>
+
 </html>
