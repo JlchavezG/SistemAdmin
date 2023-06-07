2b255939c9e00cab88838ce5698601cc38e3edda
IscJlchavezG <joseluis@iscjlchavezg.mx>
2023-06-06 19:58:11 -0600

    maquetando perfil

--- process/Perfil.php
+++ process/Perfil.php
@@ -1,19 +1,50 @@
 <div class="container mt-4">
-<div class="row justify-content-center">
-    <div class="col col-sm-10 col-md-10 col-lg-10">
-        <div class="Img-Hover">
-            <div class="row mt-2 mx-2 justify-content-center">
-                <img src="img/Users/imgUser1.png" class="img-thumbnail rounded-circle" style="width: 200px;">
+    <div class="row justify-content-center">
+        <div class="col col-sm-10 col-md-10 col-lg-10">
+            <div class="Img-Hover">
+                <div class="row mt-2 mx-2 justify-content-center">
+                    <img src="img/Users/imgUser1.png" class="img-thumbnail rounded-circle" style="width: 200px;">
+                </div>
+                <div class="row mt-4 text-center justify-content-center">
+                    <span class="display-6">Nombre completo</span>
+                </div>
+                <div class="row mt-4 text-center justify-content-center">
+                    <div class="col-sm-6 col-md-3 col-lg-3 rounded bg-success text-light me-1">
+                        <div class="row mt-1 py-2">
+                            <svg class="bi text-light" width="25" height="25" fill="currentColor">
+                                <use xlink:href="library/icons/bootstrap-icons.svg#envelope-paper-fill" />
+                            </svg>
+                        </div>
+                        <div class="row mt-1 py-1 text-center">
+                            <span>ejemplo@contacto.com</span>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-3 col-lg-3 rounded bg-success text-light me-1">
+                        <div class="row mt-1 py-2">
+                            <svg class="bi text-light" width="25" height="25" fill="currentColor">
+                                <use xlink:href="library/icons/bootstrap-icons.svg#envelope-paper-fill" />
+                            </svg>
+                        </div>
+                        <div class="row mt-1 py-1 text-center">
+                            <span>Plantel</span>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-3 col-lg-3 rounded bg-success text-light me-1">
+                        <div class="row mt-1 py-2">
+                            <svg class="bi text-light" width="25" height="25" fill="currentColor">
+                                <use xlink:href="library/icons/bootstrap-icons.svg#envelope-paper-fill" />
+                            </svg>
+                        </div>
+                        <div class="row mt-1 py-1 text-center">
+                            <span>TUsuario</span>
+                        </div>
+                    </div>
+
+
+
+                </div>
             </div>
-            <div class="row mt-4 text-center justify-content-center">
-               <div class="col-sm-6 col-md-2 col-lg-2 rounded bg-success text-light me-1">1</div>
-               <div class="col-sm-6 col-md-2 col-lg-2 rounded bg-success text-light me-1">1</div>
-               <div class="col-sm-6 col-md-2 col-lg-2 rounded bg-success text-light me-1">1</div>
-               <div class="col-sm-6 col-md-2 col-lg-2 rounded bg-success text-light me-1">1</div>
-               
-            </div>
+
         </div>
-        
     </div>
-</div>
 </div>
