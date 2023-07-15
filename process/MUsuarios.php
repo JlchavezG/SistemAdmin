c150cf5086ecd6b80e26441abbaf04d38c5bae38
IscJlchavezG <iscjlchavez@hotmail.com>
2023-07-14 23:01:38 -0600

    usuarios modulo

--- process/MUsuarios.php
+++ process/MUsuarios.php
@@ -43,6 +43,43 @@
                                 <use xlink:href="library/icons/bootstrap-icons.svg#graph-up" />
                             </svg>
                         </a>
+                        <a class="btn-outline-success rounded border-0 ms-2 py-1 ps-2 pe-2 text-decoration-none" href="#" alt="Nuevo Usuario">
+                            <svg class="bi" width="23" height="23" fill="currentColor">
+                                <use xlink:href="library/icons/bootstrap-icons.svg#question" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="container">
+            <div class="bg-light">
+                <div class="col-sm-12 col-md-12 col-lg-12 bg-light">
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead class="bg-light">
+                                <tr>
+                                    <th scope="col">Imagen</th>
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col">Apellido P</th>
+                                    <th scope="col">Apellido M</th>
+                                    <th scope="col">Telefono</th>
+                                    <th scope="col">Plantel</th>
+                                    <th scope="col">TUsuario</th>
+                                    <th scope="col">UserName</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
