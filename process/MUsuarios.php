ae48c3329c373430a0f4ad26eb93521ad3dcb998
IscJlchavezG <iscjlchavez@hotmail.com>
2023-07-14 19:53:03 -0600

    modulo usuarios

--- process/MUsuarios.php
+++ process/MUsuarios.php
@@ -3,19 +3,46 @@
         <h2 class="text-center"><span>Modulo |</span><span class="text-success"> Usuarios</span> </h2>
     </div>
     <div class="row mt-2 justify-content-center">
-        <div class="col-sm-10 col-md-10 col-lg-12 mt-1">
-            <div class="">
+        <div class="col-sm-12 col-md-12 col-lg-12 mt-1">
+            <div class="b">
                 <div class="row py-2">
-                    <div class="col-sm-5 col-md-5 col-lg-5">
+                    <div class="col-sm-12 col-md-10 col-lg-6 bg-light py-2">
                         <form action="" method="$_POST">
                         <div class="input-group input-group-sm mb-">
                             <span class="input-group-text" id="inputGroup-sizing-sm">Buscar</span>
                             <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                            <button type="submit" class="bg-success rounded border-0">
+                                <svg class="bi text-light" width="20" height="20" fill="currentColor">
+                                    <use xlink:href="library/icons/bootstrap-icons.svg#search" />
+                                </svg>
+                            </button>
                         </div>
                         </form>
                     </div>
-                    <div class="col-sm-5 col-md-5 col-lg-5">
-                        
+                    <div class="col-sm-12 col-md-10 col-lg-5 mt-3 text-center">
+                        <span>Tipos de Usuarios:</span> 
+                        <svg class="bi text-success" width="23" height="23" fill="currentColor">
+                            <use xlink:href="library/icons/bootstrap-icons.svg#person-fill-gear" />
+                        </svg>
+                        <span>0</span> 
+                        <svg class="bi text-warning" width="23" height="23" fill="currentColor">
+                            <use xlink:href="library/icons/bootstrap-icons.svg#person-check" />
+                        </svg>
+                        <span>0</span> 
+                        <svg class="bi text-secondary" width="23" height="23" fill="currentColor">
+                            <use xlink:href="library/icons/bootstrap-icons.svg#person-fill" />
+                        </svg>
+                        <span>0</span> 
+                        <a class="btn-outline-success rounded border-0 ms-4 py-1 ps-2 pe-2 text-decoration-none" href="#" alt="Nuevo Usuario">
+                            <svg class="bi" width="23" height="23" fill="currentColor">
+                                <use xlink:href="library/icons/bootstrap-icons.svg#person-plus-fill" />
+                            </svg>
+                        </a>
+                        <a class="btn-outline-success rounded border-0 ms-2 py-1 ps-2 pe-2 text-decoration-none" href="#" alt="Nuevo Usuario">
+                            <svg class="bi" width="23" height="23" fill="currentColor">
+                                <use xlink:href="library/icons/bootstrap-icons.svg#graph-up" />
+                            </svg>
+                        </a>
                     </div>
                 </div>
             </div>
