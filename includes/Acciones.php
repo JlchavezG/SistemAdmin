f45b8c927fc9a5132b402483f5e520218504b972
IscJlchavezG <iscjlchavez@hotmail.com>
2023-08-11 07:12:16 -0600

    verificar nuevo usaurio en la alerta

--- includes/Acciones.php
+++ includes/Acciones.php
@@ -336,7 +336,7 @@ else {
   EstatusUser,ImgUser)VALUES('$NewNombreR','$NewApellidoPR','$NewApellidoMR','$NewTelefonoR','$NewEmailR','$NewPlantelR','$NewTusuarioR','$NewUserNameR','$NewFechaR','$NewPasswordR','$NewOnlineR','$NewEstatusR','$NewImagenR')";
   $NewUserDataE = $ConectionBd->query($NewUserData);
   if($NewUserDataE->num_rows > 0){
-    $AccionNewU.="<div class='alert alert-success alert-dismissible fade show' role='alert'>
+    $AccionUser.="<div class='alert alert-success alert-dismissible fade show' role='alert'>
                       <strong>Excelente el Nuevo Usuario se registro con exito</strong> ElUsuario ya se encuentra registrado en la plataforma verifica por favor.
                       <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
                   </div>";
